2af5355dd47bda901b51111dcce7b4b9fc3ae62f
lijunchen <171869092@qq.com>
2022-05-09 18:23:00 +0800

    fix router

--- app/Service/Order/OrderService.php
+++ app/Service/Order/OrderService.php
@@ -1,23 +1,18 @@
 <?php
 declare(strict_types=1);
 namespace App\Service\Order;
-use App\Model\Order;
-use App\Model\OrderItem;
 use App\Model\OrderLog;
-use App\Model\ShoppingCart;
 use Hyperf\DbConnection\Db;
 use Hyperf\Di\Annotation\Inject;
 class OrderService
 {
     /**
-     * @Inject
-     * @var Order
+     * @var
      */
     protected $model;
 
     /**
-     * @Inject
-     * @var OrderItem
+     * @var
      */
     protected $item;
 
@@ -28,32 +23,9 @@ class OrderService
     protected $orderLog;
 
     /**
-     * @Inject
-     * @var ShoppingCart
+     * @var
      */
     protected $cartSerive;
-
-    /**
-     * 创建订单
-     * @param array $params
-     * @return array
-     */
-    public function unifyOrder(array $params) :array
-    {
-        if (!isset($params['ids']) || empty($params['ids'])){
-            throw new \Exception('参数错误');
-        }
-        $cart = $this->cartSerive->where(['id' => ['in', $params['ids']]])->first();
-        if (!$cart){
-            throw new \Exception('未找到购物车数据');
-        }
-        Db::beginTransaction();
-        $this->model->create();
-        $this->item->create(['order_id' => $this->model->id]);
-        $this->orderLog->create(['order_id' => $this->model->id]);
-        Db::commit();
-        return [];
-    }
 
     /**
      * 分单
